4f603776238b3b0c940201d93ae6591d3b9c1af1
Fatih <fatihhgocer>
2025-04-07 20:25:01 +0300

    Init

---
+++ src/main/java/com/aslaw_backend/AslawBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aslaw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AslawBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AslawBackendApplication.class, args);
+	}
+
+}
